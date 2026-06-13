7c261f946c6174036a9edcf023b3dbbb5516d86d
avk-ds <avik.ds2040@gmail.com>
2026-06-13 03:12:21 +0200

    My first Commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,39 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behaviour in Normal and Visual Mode
+vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- Save file
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- Quit file
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- Buffers
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<leader>x', ':Bdelete!<CR>', opts)
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts)
+
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
